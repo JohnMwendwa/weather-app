c741c5dbfa3725ff4fe7871873c974bcd8f47a4d
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2022-06-13 21:33:58 +0300

    create elements and add classNames

--- public/js/app.js
+++ public/js/app.js
@@ -15,7 +15,21 @@ form.addEventListener('submit',(e)=>{
             const error = data.error;
         }else{
             console.log(data)
-           const {city,condition,county,icon,region,temp,time} = data;   
+           const {city,condition,county,icon,region,temp,time} = data;  
+            //create elements
+            const weather_icon = document.createElement('img');
+            const weather_area = document.createElement('p')
+            const weather_condition = document.createElement('p')
+            const weather_region = document.createElement('p')
+            const weather_temp = document.createElement('p')
+            
+            // Add classes
+            weather_icon.className='weather__icon'
+            weather_area.className='weather__area'
+            weather_condition.className='weather__condition'
+            weather_region.className='weather__region'
+            weather_temp.className='weather__temperature'
+
         }
     })
 })
