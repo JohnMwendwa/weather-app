2af19550d10feba54f649a8d31c3051859d73577
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2022-06-13 07:08:03 +0300

    set app to use static assets

--- src/app.js
+++ src/app.js
@@ -1,4 +1,14 @@
+// Core Node Modules
+const path = require('path');
+
+// NPM modules
 const express = require('express');
 
 // Initialize app to use express
 const app = express();
+
+// Paths
+const staticAssetsPath = path.join(__dirname,'../public');
+
+// Set app to use static assets
+app.use(express.static(staticAssetsPath));
