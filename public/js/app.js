c6e4d6593f15a454efe321e61c1d6202367cfd5c
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2022-06-13 22:39:05 +0300

    move element creation to the top of the file

--- public/js/app.js
+++ public/js/app.js
@@ -1,35 +1,38 @@
 const form = document.querySelector('form');
 const address = document.querySelector('.search');
-const weatherContainer = document.querySelector('.weather-container')
+const weatherContainer = document.querySelector('.weather-container');
+
+
+   //create elements        
+const weather_icon = document.createElement('img');
+const weather_area = document.createElement('p');
+const weather_condition = document.createElement('p');
+const weather_country = document.createElement('p');
+const weather_region = document.createElement('p');
+const weather_temp = document.createElement('p');
+
+       // Add classes
+ weather_icon.className='weather__icon';
+ weather_area.className='weather__area';
+ weather_condition.className='weather__condition';
+ weather_country.className='weather__country';
+ weather_region.className='weather__region';
+ weather_temp.className='weather__temperature';
+
 
 form.addEventListener('submit',(e)=>{
     // prevent page from reloading
     e.preventDefault();
 
     const city = address.value;
-
+     
     fetch(`/weather?address=${city}`)
     .then(response=>response.json())
     .then(data=>{
         if(data.error){
             const error = data.error;
         }else{
-            console.log(data)
-           const {city,condition,county,icon,region,temp,time} = data;  
-            //create elements
-            const weather_icon = document.createElement('img');
-            const weather_area = document.createElement('p')
-            const weather_condition = document.createElement('p')
-            const weather_region = document.createElement('p')
-            const weather_temp = document.createElement('p')
-            
-            // Add classes
-            weather_icon.className='weather__icon'
-            weather_area.className='weather__area'
-            weather_condition.className='weather__condition'
-            weather_region.className='weather__region'
-            weather_temp.className='weather__temperature'
-
+           const {city,condition,country,icon,region,temp,time} = data.forecast;  
         }
     })
 })
