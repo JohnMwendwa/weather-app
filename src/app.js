683157e33dea8c7aad93c127e5183398fb64abed
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2022-06-13 07:30:35 +0300

    setup our first route and test if it's working

--- src/app.js
+++ src/app.js
@@ -6,9 +6,19 @@ const express = require('express');
 
 // Initialize app to use express
 const app = express();
+const port = 3000;
 
 // Paths
 const staticAssetsPath = path.join(__dirname,'../public');
 
 // Set app to use static assets
 app.use(express.static(staticAssetsPath));
+
+//ROUTES
+app.get('/',(req,res)=>{
+    res.send('Weather App')
+})
+
+app.listen(port,()=>{
+    console.log(`server started on port ${port}`)
+})
